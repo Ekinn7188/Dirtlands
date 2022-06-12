1848f1e38f27ca9d8654577e5cecf25b863875a9
ekinn7188 <ekinn7188@gmail.com>
2022-06-12 12:16:02 -0700

    Remove shopkeeper data on npc despawn

--- src/main/java/net/dirtlands/listeners/shopkeepers/NPCDeletion.java
+++ src/main/java/net/dirtlands/listeners/shopkeepers/NPCDeletion.java
@@ -3,8 +3,6 @@ package net.dirtlands.listeners.shopkeepers;
 import dirtlands.db.Tables;
 import net.citizensnpcs.api.event.NPCDespawnEvent;
 import net.dirtlands.Main;
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -12,8 +10,6 @@ public class NPCDeletion implements Listener {
 
     @EventHandler
     public void onNPCDespawn(NPCDespawnEvent e) {
-        Bukkit.broadcast(Component.text("NPC Despawned: " + e.getNPC().getId()));
-
         Main.getPlugin().getDslContext().deleteFrom(Tables.SHOPKEEPERS)
                 .where(Tables.SHOPKEEPERS.SHOPKEEPERID.eq(e.getNPC().getId())).execute();
 
